bcf3c2314d5e39284863a1b2e28315e160534e01
Aaron Kriegman <aaronkplus2@gmail.com>
2024-12-11 11:40:38 -0800

    no message

--- model.py
+++ model.py
@@ -60,6 +60,7 @@ class CausalSelfAttention(nn.Module):
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         self.dropout = config.dropout
+        print(f"Attention activation type: {config.attention_activation}")
         # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
         self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention') and config.attention_activation == 'softmax'
         if not self.flash:
